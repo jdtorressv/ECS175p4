f790c57f47bd5f80fc5e5f1b39970c65ee7f7bd0
Joseph Torres <jdtorres@ucdavis.edu>
2018-12-12 19:12:56 -0800

    Casteljau working, insert function not due to glutpostredisplay

--- main.cpp
+++ main.cpp
@@ -106,26 +106,76 @@ void populateCurves()
 				b2.pointArr.push_back(pt);
 		}
 }
+Point getNextCastelPt(int s, int j, float t, BezSpline spline)
+{
+		if (s == 0)
+				return spline.pointArr.at(j);
+		Point p1 = getNextCastelPt(s - 1, j, t, spline);
+		Point p2 = getNextCastelPt(s - 1, j + 1, t, spline);
+		Point nextPt(((1-t) * p1.x + t * p2.x), ((1-t) * p1.y + t * p2.y));
+		return nextPt;
+}
+void casteljau(BezSpline spline, float resolution)
+{
+		glBegin(GL_POINTS);
+
+				Point currPt(0, 0);
+				float t = 0;
+				while (t <= 1.0)
+				{
+					currPt = getNextCastelPt(spline.pointArr.size() - 1, 0, t, spline);
+					glVertex2f(currPt.x, currPt.y);
+					t += resolution;
+				}
+
+		glEnd();
+
+		glFlush();
+}
+
 void cntrlPolyBez1()
 {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
+
 		glBegin(GL_LINES);
 
-		glColor3f(.96, .38, .53);
-
-		for (int i = 0; i < bez1.pointArr.size()-1; i++) {
-				glVertex2f(bez1.pointArr.at(i).x, bez1.pointArr.at(i).y);
-				glVertex2f(bez1.pointArr.at(i+1).x, bez1.pointArr.at(i+1).y);
-		}
+				glColor3f(.04, .15, 1);
+				glVertex2f(-1,0);
+				glVertex2f(1,0);
+				glVertex2f(0,-1);
+				glVertex2f(0,1);
 
 		glEnd();
+
+		glBegin(GL_LINES);
+
+				glColor3f(.96, .38, .53);
+				for (int i = 0; i < bez1.pointArr.size()-1; i++) {
+						glVertex2f(bez1.pointArr.at(i).x, bez1.pointArr.at(i).y);
+						glVertex2f(bez1.pointArr.at(i+1).x, bez1.pointArr.at(i+1).y);
+				}
+
+		glEnd();
+
 		glFlush();
 }
 void cntrlPolyBez2()
 {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
+
+		glBegin(GL_LINES);
+
+			glColor3f(.04, .15, 1);
+			glVertex2f(-1,0);
+			glVertex2f(1,0);
+			glVertex2f(0,-1);
+			glVertex2f(0,1);
+
+
+		glEnd();
+
 		glBegin(GL_LINES);
 
 		glColor3f(.96, .38, .53);
@@ -141,6 +191,18 @@ void cntrlPolyB1()
 {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
+
+		glBegin(GL_LINES);
+
+			glColor3f(.04, .15, 1);
+			glVertex2f(-1,0);
+			glVertex2f(1,0);
+			glVertex2f(0,-1);
+			glVertex2f(0,1);
+
+
+		glEnd();
+
 		glBegin(GL_LINES);
 
 		glColor3f(.96, .38, .53);
@@ -155,6 +217,18 @@ void cntrlPolyB2()
 {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();
+
+		glBegin(GL_LINES);
+
+			glColor3f(.04, .15, 1);
+			glVertex2f(-1,0);
+			glVertex2f(1,0);
+			glVertex2f(0,-1);
+			glVertex2f(0,1);
+
+
+		glEnd();
+
 		glBegin(GL_LINES);
 
 		glColor3f(.96, .38, .53);
@@ -171,80 +245,36 @@ void cntrlPolyB2()
 void init()
 {
 	if (glutGetWindow() == windowID)
-		glClearColor(1.0, 1.0, 1.0, 0.0); //Set color to white
+			glClearColor(1.0, 1.0, 1.0, 0.0); //Set color to white
 	else
-        	glClearColor(0.0, 0.0, 0.0, 0.0); //Set color to black
+      glClearColor(0.0, 0.0, 0.0, 0.0); //Set color to black
 
-        glMatrixMode(GL_PROJECTION);
+  glMatrixMode(GL_PROJECTION);
 }
 
 //Draws the lines as specified by the lines via vertex pairs in the input file; XY: All Z values are ignored
 void drawSceneBez1()
 {
-	//	glClear(GL_COLOR_BUFFER_BIT);
-  //	glLoadIdentity();
-		glBegin(GL_LINES);
-
-			glColor3f(.04, .15, 1);
-			glVertex2f(-1,0);
-			glVertex2f(1,0);
-			glVertex2f(0,-1);
-			glVertex2f(0,1);
-
-
-
-	glEnd();
-	glFlush();
+		casteljau(bez1, bez1Res);
 }
 //Draws the lines as specified by the lines via vertex pairs in the input file; XZ: All Y values are ignored
 void drawSceneBez2()
 {
-      //  glClear(GL_COLOR_BUFFER_BIT);
-      //  glLoadIdentity();
-	glBegin(GL_LINES);
-
-					glColor3f(.04, .15, 1);
-					glVertex2f(-1,0);
-					glVertex2f(1,0);
-					glVertex2f(0,-1);
-					glVertex2f(0,1);
-
-	glEnd();
-	glFlush();
+		casteljau(bez2, bez2Res);
 }
 //Draws the lines as specified by the lines via vertex pairs in the input file; XZ: All Y values are ignored
 void drawSceneB1()
 {
-      //  glClear(GL_COLOR_BUFFER_BIT);
-      //  glLoadIdentity();
-				glBegin(GL_LINES);
-
-					glColor3f(.04, .15, 1);
-					glVertex2f(-1,0);
-					glVertex2f(1,0);
-					glVertex2f(0,-1);
-					glVertex2f(0,1);
 
 
-	glEnd();
-	glFlush();
+
 }
 
 void drawSceneB2()
 {
-        //glClear(GL_COLOR_BUFFER_BIT);
-        //glLoadIdentity();
-				glBegin(GL_LINES);
-
-					glColor3f(.04, .15, 1);
-					glVertex2f(-1,0);
-					glVertex2f(1,0);
-					glVertex2f(0,-1);
-					glVertex2f(0,1);
 
 
-	glEnd();
-	glFlush();
+
 }
 //Set up main display
 void background()
@@ -256,6 +286,7 @@ void background()
 //Master display function
 void display()
 {
+				cout << "I'm being called!\n";
 				glutSetWindow(windowID);
 				background();
 
@@ -462,12 +493,71 @@ void rotateMenu(int pid)
                         file << lArr.at(i).at(k) << " " << lArr.at(i).at(k+1) << '\n';
         }
 }*/
-void mainMenu(int pid)
+void insertBezPt(BezSpline spline)
+{
+			float xVal;
+			float yVal;
+			cout << "Please enter the x and y coordinates for the control point you would like to add\n";
+			cin >> xVal >> yVal;
+			if (fabs(xVal) > NORM || fabs(yVal) > NORM) {
+					cout << "Values entered must be in the range [-500.0, 500.0]\n";
+					exit(ERROR);
+			}
+			Point pt(xVal / NORM, yVal / NORM);
+			spline.pointArr.push_back(pt);
+
+			glutPostRedisplay();
+}
+void deleteBezPt(BezSpline spline)
 {
 
+}
+void modifyBezPt(BezSpline spline)
+{
 
+}
 
-
+void mainMenu(int pid)
+{
+		switch(pid)
+		{
+					case 0: // Insert Bez1
+							insertBezPt(bez1);
+							break;
+					case 1: // Delete Bez1
+							deleteBezPt(bez1);
+							break;
+					case 2: // Modify Bez1
+							modifyBezPt(bez1);
+							break;
+					case 3: // Insert Bez2
+							insertBezPt(bez2);
+							break;
+					case 4: // Delete Bez2
+							deleteBezPt(bez2);
+							break;
+					case 5: // Modify Bez2
+							modifyBezPt(bez2);
+							break;
+					case 6: // Insert B1
+							//
+							break;
+					case 7: // Delete B1
+							//
+							break;
+					case 8: // Modify B1
+							//
+							break;
+					case 9: // Insert B2
+							//
+							break;
+					case 10: // Delete B2
+							//
+							break;
+					case 11: // Modify B2
+							//
+							break;
+		}
 }
 
 int main(int argc, char** argv)
@@ -508,37 +598,36 @@ int main(int argc, char** argv)
 	windowB2 = glutCreateSubWindow(windowID, 450, 475, 350, 350);
 	init();
 
-				glutSetWindow(windowBez1);
-        glutCreateMenu(mainMenu);
-								glutAddMenuEntry("Insert Control Point", 0);
-								glutAddMenuEntry("Delete Control Point", 1);
-								glutAddMenuEntry("Modify Control Point", 2);
-        glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutSetWindow(windowBez1);
+  glutCreateMenu(mainMenu);
+					glutAddMenuEntry("Insert Control Point", 0);
+					glutAddMenuEntry("Delete Control Point", 1);
+					glutAddMenuEntry("Modify Control Point", 2);
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-			  glutSetWindow(windowBez2);
-				glutCreateMenu(mainMenu);
-								glutAddMenuEntry("Insert Control Point", 3);
-								glutAddMenuEntry("Delete Control Point", 4);
-								glutAddMenuEntry("Modify Control Point", 5);
-				glutAttachMenu(GLUT_RIGHT_BUTTON);
+  glutSetWindow(windowBez2);
+	glutCreateMenu(mainMenu);
+					glutAddMenuEntry("Insert Control Point", 3);
+					glutAddMenuEntry("Delete Control Point", 4);
+					glutAddMenuEntry("Modify Control Point", 5);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-				glutSetWindow(windowB1);
-				glutCreateMenu(mainMenu);
-								glutAddMenuEntry("Insert Control Point", 6);
-								glutAddMenuEntry("Delete Control Point", 7);
-								glutAddMenuEntry("Modify Control Point", 8);
-				glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutSetWindow(windowB1);
+	glutCreateMenu(mainMenu);
+					glutAddMenuEntry("Insert Control Point", 6);
+					glutAddMenuEntry("Delete Control Point", 7);
+					glutAddMenuEntry("Modify Control Point", 8);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-				glutSetWindow(windowB2);
-				glutCreateMenu(mainMenu);
-								glutAddMenuEntry("Insert Control Point", 9);
-								glutAddMenuEntry("Delete Control Point", 10);
-								glutAddMenuEntry("Modify Control Point", 11);
-				glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutSetWindow(windowB2);
+	glutCreateMenu(mainMenu);
+					glutAddMenuEntry("Insert Control Point", 9);
+					glutAddMenuEntry("Delete Control Point", 10);
+					glutAddMenuEntry("Modify Control Point", 11);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 
-        glutDisplayFunc(display);
-
+  glutDisplayFunc(display);
 
 	glutMainLoop();
 
